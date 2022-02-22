440780c91e3cc1241e87839271b930b4a7430524
lfilipova <lucijaf44@gmail.com>
2022-02-22 23:16:54 +0100

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GrocerySotre.MvcApplication" Language="C#" %>
---
+++ ViewModel/OrderDetailsModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GrocerySotre.ViewModel
+{
+    public class OrderDetailsModel
+    {
+        public int OrderDetailId { get; set; }
+        public int OrderId { get; set; }
+        public Guid ItemId { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Total { get; set; }
+    }
+}
